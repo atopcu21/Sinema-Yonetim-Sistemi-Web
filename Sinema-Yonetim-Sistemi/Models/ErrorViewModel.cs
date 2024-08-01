ebe6ef7d42276d96d4a97c378361f0c17e3c9be2
merve.tasci <merve.tasci@MERVETASCIDELL>
2024-08-01 14:36:38 +0300

    Proje dosyası ekle.

---
+++ Sinema-Yonetim-Sistemi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sinema_Yonetim_Sistemi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
